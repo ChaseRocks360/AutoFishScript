a388b4c1d05b370e02dd9f3b1aceb5ea3bfc3722
Chase <100978029+ChaseRocks360@users.noreply.github.com>
2024-07-18 14:50:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def capture_screen():
     return screenshot
 
 # Function to check for the specific color
-def contains_color(image, target_color):
+def contains_target_color(image, target_color=(231, 84, 73)):
     pixels = image.load()
     width, height = image.size
 
@@ -21,9 +21,6 @@ def contains_color(image, target_color):
     return False
 
 # Main loop
-color1 = (255, 85, 85)  # RGB value for #FF5555
-color2 = (63, 21, 21)   # RGB value for #3F1515
-
 while True:
     if keyboard.is_pressed('F7'):  # Check if F7 is pressed to stop the script
         print("F7 pressed. Exiting script.")
@@ -31,21 +28,17 @@ while True:
 
     image = capture_screen()
 
-    # Check for both colors
-    color1_found = contains_color(image, color1)
-    color2_found = contains_color(image, color2)
-
-    if color1_found and color2_found:
+    if contains_target_color(image):
         # First right-click
         pyautogui.click(button='right')
-        print("Both colors detected. First right-click performed, waiting 5 milliseconds before the next click...")
+        print("First right-click detected, waiting 5 milliseconds before the next click...")
 
         # Wait 5 milliseconds
         time.sleep(0.005)
 
         # Second right-click
         pyautogui.click(button='right')
-        print("Second right-click performed, waiting 500 milliseconds before resuming detection...")
+        print("Second right-click detected, waiting 500 milliseconds before resuming detection...")
 
         # Wait 500 milliseconds after the second right-click
         time.sleep(0.5)
